72342fcce39e51fdfa45853cadc9ff3a9fbf54d7
jose A <rodriguezjoseagustin31@gmail.com>
2021-07-19 17:19:32 -0400

    make it better

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,26 +1,7 @@
 const router = require('express').Router();
 
-const {
-	sendEmailBudget,
-	sendEmailContact,
-	renderIndex,
-	renderAbout,
-	renderBudget,
-} = require('../controllers/index.controllers');
-
-const {
-	formContactIsAuthenticated,
-	formBudgetIsAuthenticated,
-} = require('../helpers/auth');
+const { renderIndex } = require('../controllers/index.controllers');
 
 router.get('/', renderIndex);
-
-router.get('/about', renderAbout);
-
-router.get('/budget', renderBudget);
-
-router.post('/send-email', formContactIsAuthenticated, sendEmailContact);
-
-router.post('/send-budget', formBudgetIsAuthenticated, sendEmailBudget);
 
 module.exports = router;
