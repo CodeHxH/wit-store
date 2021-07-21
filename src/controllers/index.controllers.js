517be91a457becacfa50f197def1cb6ec4e2d88c
jose A <rodriguezjoseagustin31@gmail.com>
2021-07-21 17:48:49 -0400

    home

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,7 +1,44 @@
 const indexCtrl = {};
 
 indexCtrl.renderIndex = (req, res) => {
-	res.render('index');
+	const products = [
+		{
+			price: 8.0,
+			name: 'Camiseta Lost in Paradise',
+			description:
+				'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
+			img: '/img/product.jpg',
+		},
+		{
+			price: 8.0,
+			name: 'Camiseta Lost in Paradise',
+			description:
+				'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
+			img: '/img/product1.jpg',
+		},
+		{
+			price: 8.0,
+			name: 'Camiseta Lost in Paradise',
+			description:
+				'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
+			img: '/img/product2.jpg',
+		},
+		{
+			price: 8.0,
+			name: 'Camiseta Lost in Paradise',
+			description:
+				'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
+			img: '/img/product3.jpg',
+		},
+		{
+			price: 8.0,
+			name: 'Camiseta Lost in Paradise',
+			description:
+				'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
+			img: '/img/product4.jpg',
+		},
+	];
+	res.render('index', { products });
 };
 
 module.exports = indexCtrl;
---
+++ src/public/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.carrousel').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		autoplay: true,
+		arrows: false,
+		autoplaySpeed: 5000,
+	});
+});
