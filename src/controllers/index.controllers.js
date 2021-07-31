67a453a2847c36ec2b02b5446349e74745b4a700
jose A <rodriguezjoseagustin31@gmail.com>
2021-07-30 21:09:49 -0400

    login

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -2,13 +2,19 @@ const indexCtrl = {};
 
 const products = [
 	{
-		price: 10.0,
+		id: 10,
+		type: 'ropa',
+		labels: 'clothing',
 		name: 'Camiseta Lost in Paradise',
+		price: 10.0,
 		description:
 			'Camiseta básica, 50% algodón 50% poliéster, bordado reflejante, hecha en USA.',
 		img: '/img/product.jpg',
 	},
 	{
+		id: 10,
+		type: 'juegos',
+		labels: 'clothing',
 		price: 50.0,
 		name: 'Mario Kart 8 Deluxe',
 		description:
@@ -16,6 +22,9 @@ const products = [
 		img: '/img/MarioKart.png',
 	},
 	{
+		id: 10,
+		type: 'ropa',
+		labels: 'clothing',
 		price: 8.0,
 		name: 'Camiseta To The Top',
 		description:
@@ -23,6 +32,9 @@ const products = [
 		img: '/img/product2.jpg',
 	},
 	{
+		id: 10,
+		type: 'juegos',
+		labels: 'clothing',
 		price: 50.0,
 		name: 'Super Mario Odyssey',
 		description:
@@ -30,6 +42,9 @@ const products = [
 		img: '/img/MarioOdyssey.png',
 	},
 	{
+		id: 10,
+		type: 'juegos',
+		labels: 'clothing',
 		price: 60.0,
 		name: 'Smash Bros Ultimate',
 		description:
@@ -44,6 +59,14 @@ indexCtrl.renderIndex = (req, res) => {
 
 indexCtrl.renderProducts = (req, res) => {
 	res.render('products', { products });
+};
+
+indexCtrl.renderSingin = (req, res) => {
+	res.render('singin');
+};
+
+indexCtrl.renderSingup = (req, res) => {
+	res.render('singup');
 };
 
 module.exports = indexCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,10 +3,16 @@ const router = require('express').Router();
 const {
 	renderIndex,
 	renderProducts,
+	renderSingin,
+	renderSingup,
 } = require('../controllers/index.controllers');
 
 router.get('/', renderIndex);
 
 router.get('/products', renderProducts);
+
+router.get('/singin', renderSingin);
+
+router.get('/singup', renderSingup);
 
 module.exports = router;
